f90de33e9d457ca1f9d68b191c4edc461b43e6c1
smchejolan <48845905+smchejolan@users.noreply.github.com>
2020-08-28 20:26:47 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# TGC - MonoGame - TP
+[![BCH compliance](https://bettercodehub.com/edge/badge/tgc-utn/tgc-monogame-tp?branch=master)](https://bettercodehub.com/)
+[![GitHub license](https://img.shields.io/github/license/tgc-utn/tgc-monogame-tp.svg)](https://github.com/tgc-utn/tgc-monogame-tp/blob/master/LICENSE)
+
+[#BuiltWithMonoGame](http://www.monogame.net) and [.NET Core](https://dotnet.microsoft.com)
+
+## Descripción
+Proyecto plantilla para los trabajos prácticos de la asignatura electiva [Técnicas de Gráficos por Computadora](http://tgc-utn.github.io/) (TGC) en la carrera de Ingeniería en Sistemas de Información. Universidad Tecnológica Nacional, Facultad Regional Buenos Aires (UTN-FRBA).
+
+## Requisitos
+* [.NET Core SDK](https://docs.microsoft.com/dotnet/core/install/sdk)
+* El IDE que prefieran:
+  * [Visual Studio Code](https://code.visualstudio.com)
+  * [Visual Studio](https://visualstudio.microsoft.com/es/vs) o [Visual Studio for Mac](https://visualstudio.microsoft.com/es/vs/mac)
+  * [Rider](https://www.jetbrains.com/rider)
+* [MGCB Editor](https://docs.monogame.net/articles/tools/mgcb_editor.html)
+* [MonoGame.Framework.DesktopGL](https://www.nuget.org/packages/MonoGame.Framework.DesktopGL) (Se baja automáticamente al hacer build por primera vez)
+
+## Configuración del entorno de desarrollo
+ * [Windows 10](https://docs.monogame.net/articles/getting_started/1_setting_up_your_development_environment_windows.html)
+   * Se puede usar Visual Studio Code o Rider. La documentación oficial solo explica Visual Studio, pero cada uno puede configurar el que les sea más cómodo.
+ * [Linux (probado en Ubuntu 20.04)](https://docs.monogame.net/articles/getting_started/1_setting_up_your_development_environment_ubuntu.html)
+ * [Mac (probado en macOS Catalina)](https://docs.monogame.net/articles/getting_started/1_setting_up_your_development_environment_macos.html)
+
+Afuera del mundo Windows, vas a necesitar la ayudar de [Wine](https://www.winehq.org) para los shaders, por lo menos por [ahora](https://github.com/MonoGame/MonoGame/issues/2167).
+
+Los recursos usados se almacenan utilizando [Git LFS](https://git-lfs.github.com), con lo cual antes de hacer clone del repositorio les conviene tenerlo instalado así es automático el pull o si ya lo tienen pueden hacer ```git lfs pull```.
+
+## Integrantes
+TGC, Tgcito  |  Apellido, Nombre
+------------ | -------------
+<img src="https://github.com/tgc-utn/tgc-utn.github.io/blob/master/images/robotgc.png" height="500"> | <img src="https://github.com/tgc-utn/tgc-utn.github.io/blob/master/images/trofeotp.png" height="500">
+
+## Capturas
+![screenshot1](https://github.com/tgc-utn/tgc-monogame-tp/blob/master/TGC.MonoGame.TP/Icon.bmp)
+![screenshot2](https://github.com/tgc-utn/tgc-monogame-tp/blob/master/TGC.MonoGame.TP/Icon.bmp)
+![screenshot3](https://github.com/tgc-utn/tgc-monogame-tp/blob/master/TGC.MonoGame.TP/Icon.bmp)
+![screenshot4](https://github.com/tgc-utn/tgc-monogame-tp/blob/master/TGC.MonoGame.TP/Icon.bmp)
+![screenshot5](https://github.com/tgc-utn/tgc-monogame-tp/blob/master/TGC.MonoGame.TP/Icon.bmp)
+
+## Game Play
+[![Watch the video](https://img.youtube.com/vi/pgEwUC0jvH4/0.jpg)](https://www.youtube.com/playlist?list=PLRM4L32DjvnazuMl8wZlbpEYL5Qh63ulG)
---
+++ TGC.MonoGame.TP/TGCGame.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace TGC.MonoGame.TP
+{
+    /// <summary>
+    ///     Esta es la clase principal  del juego.
+    ///     Inicialmente puede ser renombrado o copiado para hacer más ejemplos chicos, en el caso de copiar para que se
+    ///     ejecute el nuevo ejemplo deben cambiar la clase que ejecuta Program <see cref="Program.Main()" /> linea 10.
+    /// </summary>
+    public class TGCGame : Game
+    {
+        public const string ContentFolder3D = "Models/";
+        public const string ContentFolderEffect = "Effects/";
+        public const string ContentFolderMusic = "Music/";
+        public const string ContentFolderSounds = "Sounds/";
+        public const string ContentFolderSpriteFonts = "SpriteFonts/";
+        public const string ContentFolderTextures = "Textures/";
+
+        /// <summary>
+        ///     Constructor del juego.
+        /// </summary>
+        public TGCGame()
+        {
+            // Maneja la configuracion y la administracion del dispositivo grafico.
+            Graphics = new GraphicsDeviceManager(this);
+            // Descomentar para que el juego sea pantalla completa.
+            // Graphics.IsFullScreen = true;
+            // Carpeta raiz donde va a estar toda la Media.
+            Content.RootDirectory = "Content";
+            // Hace que el mouse sea visible.
+            IsMouseVisible = true;
+        }
+
+        private GraphicsDeviceManager Graphics { get; }
+        private SpriteBatch SpriteBatch { get; set; }
+        private Model Model { get; set; }
+        private float Rotation { get; set; }
+        private Matrix World { get; set; }
+        private Matrix View { get; set; }
+        private Matrix Projection { get; set; }
+
+        /// <summary>
+        ///     Se llama una sola vez, al principio cuando se ejecuta el ejemplo.
+        ///     Escribir aquí todo el código de inicialización: todo procesamiento que podemos pre calcular para nuestro juego.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // La logica de inicializacion que no depende del contenido se recomienda poner en este metodo.
+
+            // Apago el backface culling.
+            // Esto se hace por un problema en el diseno del modelo del logo de la materia.
+            // Una vez que empiecen su juego, esto no es mas necesario y lo pueden sacar.
+            var rasterizerState = new RasterizerState();
+            rasterizerState.CullMode = CullMode.None;
+            GraphicsDevice.RasterizerState = rasterizerState;
+            // Seria hasta aca.
+
+            // Configuramos nuestras matrices de la escena.
+            World = Matrix.CreateRotationY(MathHelper.Pi);
+            View = Matrix.CreateLookAt(Vector3.UnitZ * 150, Vector3.Zero, Vector3.Up);
+            Projection =
+                Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, GraphicsDevice.Viewport.AspectRatio, 1, 250);
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        ///     Se llama una sola vez, al principio cuando se ejecuta el ejemplo, despues de Initialize.
+        ///     Escribir aqui el codigo de inicializacion: cargar modelos, texturas, estructuras de optimizacion, el
+        ///     procesamiento que podemos pre calcular para nuestro juego.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Aca es donde deberiamos cargar todos los contenido necesarios antes de iniciar el juego.
+            SpriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // Cargo el modelo del logo.
+            Model = Content.Load<Model>(ContentFolder3D + "tgc-logo/tgc-logo");
+            // Obtengo su efecto para cambiarle el color y activar la luz predeterminada que tiene MonoGame.
+            var modelEffect = (BasicEffect) Model.Meshes[0].Effects[0];
+            modelEffect.DiffuseColor = Color.DarkBlue.ToVector3();
+            modelEffect.EnableDefaultLighting();
+
+            base.LoadContent();
+        }
+
+        /// <summary>
+        ///     Se llama en cada frame.
+        ///     Se debe escribir toda la lógica de computo del modelo, así como también verificar entradas del usuario y reacciones
+        ///     ante ellas.
+        /// </summary>
+        protected override void Update(GameTime gameTime)
+        {
+            // Aca deberiamos poner toda la logica de actualizacion del juego.
+
+            // Capturar Input teclado
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+                //Salgo del juego.
+                Exit();
+
+            // Basado en el tiempo que paso se va generando una rotacion.
+            Rotation += Convert.ToSingle(gameTime.ElapsedGameTime.TotalSeconds);
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        ///     Se llama cada vez que hay que refrescar la pantalla.
+        ///     Escribir aquí todo el código referido al renderizado.
+        /// </summary>
+        protected override void Draw(GameTime gameTime)
+        {
+            // Aca deberiamos poner toda la logia de renderizado del juego.
+            GraphicsDevice.Clear(Color.Black);
+
+            //Finalmente invocamos al draw del modelo.
+            Model.Draw(World * Matrix.CreateRotationY(Rotation), View, Projection);
+
+            base.Draw(gameTime);
+        }
+
+        /// <summary>
+        ///     Libero los recursos que se cargaron en el juego.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // Libero los recursos.
+            Content.Unload();
+
+            base.UnloadContent();
+        }
+    }
+}
